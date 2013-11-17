9bce26703b93aaa5b454453fba110a5f0bc6f7ac
ken61502 <kennyson@gmail.com>
2013-11-17 15:01:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KennySungInfo::Application.config.secret_token = '2dac24b331e7e10964e0fe7c532903e031edad20c7e0305a2ce72311308202316d2fce5588375731af598bbdc7b3f715b9ca3312058ae7e309b9892b437ea5af'
